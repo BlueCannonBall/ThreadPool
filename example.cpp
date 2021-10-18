751f8c3f0d79e1915a2b9da6c2f575af74431e66
BlueCannonBall <mmttiizz100@gmail.com>
2021-10-18 18:31:46 -0400

    Readme

--- example.cpp
+++ example.cpp
@@ -3,9 +3,9 @@
 #include <mutex>
 
 int main() {
-    tp::ThreadPool pool;
+    tp::ThreadPool pool; // Create a threadpool
     std::mutex mtx;
-    std::vector<std::shared_ptr<tp::Task>> tasks;
+    std::vector<std::shared_ptr<tp::Task>> tasks; // Vector to store threadpool tasks
 
     for (unsigned int i = 0; i < 10; i++) {
         tasks.push_back(pool.schedule([i, &mtx]() {
@@ -14,10 +14,10 @@ int main() {
                 std::cout << "Printing from task: " << i << std::endl;
                 mtx.unlock();
             }
-        }));
+        })); // Schedule tasks and add them to the vector
     }
 
-    for (auto task : tasks) {
+    for (auto task : tasks) { // Wait on every task
         task->await();
     }
     std::cout << "All tasks done!" << std::endl;
