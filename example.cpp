45f71b5a6bae72b8e928f4065f11fb0f3ed32c76
BlueCannonBall <mmttiizz100@gmail.com>
2021-10-20 19:07:41 -0400

    Optimize threadpool

--- example.cpp
+++ example.cpp
@@ -8,13 +8,13 @@ int main() {
     std::vector<std::shared_ptr<tp::Task>> tasks; // Vector to store threadpool tasks
 
     for (unsigned int i = 0; i < 10; i++) {
-        tasks.push_back(pool.schedule([i, &mtx](void*) {
+        tasks.push_back(std::move(pool.schedule([i, &mtx](void*) {
             if (true) {
                 mtx.lock();
                 std::cout << "Printing from task: " << i << std::endl;
                 mtx.unlock();
             }
-        })); // Schedule tasks and add them to the vector
+        }))); // Schedule tasks and add them to the vector
     }
 
     for (auto task : tasks) { // Wait on every task
