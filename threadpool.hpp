58f7dd8fbe87053f6056c430cdf00fe0ebf47d28
BlueCannonBall <mmttiizz100@gmail.com>
2023-06-11 19:21:24 -0400

    update

--- threadpool.hpp
+++ threadpool.hpp
@@ -117,7 +117,7 @@ namespace tp {
 
     public:
         ThreadPool(unsigned int pool_size = std::thread::hardware_concurrency()) {
-            for (unsigned int i = 0; i < pool_size; i++) {
+            for (unsigned int i = 0; i < pool_size; ++i) {
                 auto new_queue = new CommandQueue;
                 std::thread new_thread(&ThreadPool::runner, this, new_queue);
                 threads.push_back({std::move(new_thread), new_queue});
@@ -177,7 +177,7 @@ namespace tp {
 
                 threads.resize(new_pool_size);
             } else {
-                for (unsigned int i = threads.size(); i < new_pool_size; i++) {
+                for (unsigned int i = threads.size(); i < new_pool_size; ++i) {
                     auto new_queue = new CommandQueue;
                     std::thread new_thread(&ThreadPool::runner, this, new_queue);
                     threads.push_back({std::move(new_thread), new_queue});
